dae2266d911c8497634815805b68cc6f29ac6853
Danilo Maccari <maccari78@gmail.com>
2024-09-22 19:37:30 -0300

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 #   
-admin_email = 'maccari78@gmail.com'
-admin_password = 'n0m3nN3sc10'
+admin_email = 'admin@example.com'
+admin_password = 'password'
 
 unless User.exists?(email: admin_email)
   User.create!(email: admin_email, password: admin_password, admin: true)
